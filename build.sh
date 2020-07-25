0ba13ce6b2ab83cde83723c04018edec1b0472a8
jld3103 <jld3103yt@gmail.com>
2020-07-25 10:38:49 +0200

    Switch from cherry-picking to diffing and revert incompatible commits from Android Mainline

--- build.sh
+++ build.sh
@@ -18,6 +18,7 @@ if [ ! -d .git ]; then
   git init
   git config pull.rebase true
   git config merge.renamelimit 10000
+  git config diff.renameLimit 10000
   git remote add aosp https://android.googlesource.com/kernel/common
   git remote add linux https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux
   git remote add msm8916 https://github.com/msm8916-mainline/linux
@@ -28,34 +29,33 @@ git clean -fd
 
 git fetch --all --tags --progress
 
-git checkout --track -b aosp aosp/android-mainline 2>/dev/null || true
-git checkout aosp
-git pull -f
 git checkout --track -b linux linux/master 2>/dev/null || true
 git checkout linux
 git pull -f
 git checkout --track -b msm8916 msm8916/master 2>/dev/null || true
 git checkout msm8916
 git pull -f
-
 msm8916_head=$(git rev-parse HEAD)
-merge_base=$(git merge-base msm8916 linux)
+git checkout --track -b aosp aosp/android-mainline 2>/dev/null || true
 git checkout aosp
-aosp_head=$(git rev-parse HEAD)
-if [ "$(git diff "$msm8916_head"~ "$msm8916_head" | sha256sum)" != "$(git diff "$aosp_head"~ "$aosp_head" | sha256sum)" ] &&
-  [ "$(git diff "$msm8916_head"~ "$msm8916_head" | sha256sum)" != "$(git diff "$aosp_head"~2 "$aosp_head"~ | sha256sum)" ]; then
-  git reset --hard aosp/android-mainline >/dev/null 2>&1 || true
-  git cherry-pick --abort || true
-  git cherry-pick "$merge_base...$msm8916_head" -X theirs --ff
-  until git cherry-pick --continue; do
-    git commit --allow-empty --no-edit
-  done
-fi
+git pull -f
 
-if [ "$(git diff "$msm8916_head"~ "$msm8916_head" | sha256sum)" == "$(git diff "$aosp_head"~2 "$aosp_head"~ | sha256sum)" ]; then
-  git reset --hard @~
-fi
-git apply ../*.patch
+msm8916_merge_base=$(git merge-base msm8916 linux)
+aosp_merge_base=$(git merge-base aosp linux)
+
+git reset --hard aosp/android-mainline >/dev/null 2>&1 || true
+
+git diff "$msm8916_merge_base...$aosp_merge_base" >patch.txt
+git apply -R --reject patch.txt
+rm patch.txt
+git add .
+git commit -m "Revert (possibly) incompatible commits from android-mainline"
+
+git diff "$msm8916_merge_base...$msm8916_head" >patch.txt
+git apply --reject patch.txt
+rm patch.txt
+git add .
+git commit -m "Add commits from msm8916"
 
 ../merge_config_intelligently.sh \
   arch/arm64/configs/paella_defconfig \
@@ -66,3 +66,5 @@ git apply ../*.patch
 sed -i "s/=m/=y/" arch/arm64/configs/paella_defconfig
 git add .
 git commit -m "Add paella_defconfig"
+
+# Patches not included, because it might work now
